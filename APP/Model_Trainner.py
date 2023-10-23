870a8e37c4244291fb0ef22ec9e636251eadd3af
Eduardo <trejo.alfredo.es@outlook.com>
2023-10-22 22:10:48 -0600

    Setfunction
    At trainner scrip class all atributs set function created

--- APP/Model_Trainner.py
+++ APP/Model_Trainner.py
@@ -7,7 +7,6 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 
-
 from PyQt5.QtCore import *
 
 
@@ -18,6 +17,33 @@ class DL_Trainner(QThread):
 
     def __init__(self):
         super().__init__()
+        self.trainer_model = Model_Trainer()
+        
+    def SetColumToForcast(self,val):
+        self.ColumToforcast=val
+        
+    def SetnumEpochs(self,val):
+        self.numEpochs=val
+        
+    def SetModel_Path(self,val):
+        self.Model_Path=val
+        
+    def SetData_CSV(self,val):
+        self.Data_CSV=val
+    
+    def SetpercentageData(self,val):
+        self.percentageData=val
+    def SetNp_pasdays(self,val):
+        self.Np_pasdays=val
     
     def run(self):
         print("Hello world trainnig Scrip running :)")
+        #is this one the old working trainer versi
+        ColumToforcast=self.ColumToforcast
+        numEpochs=self.numEpochs
+        Model_Path=self.Model_Path
+        Data_CSV=self.Data_CSV
+        percentageData=self.percentageData
+        Np_pasdays=self.Np_pasdays
+        training_result=self.trainer_model.to_train(ColumToforcast,numEpochs,Model_Path,Data_CSV,percentageData,Np_pasdays)
+        
